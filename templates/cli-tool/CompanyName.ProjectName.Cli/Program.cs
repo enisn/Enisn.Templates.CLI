06617598b34db39f2442900db7c4cd451c58e428
Enis Necipoglu <iletisim@enisnecipoglu.com>
2024-08-14 15:08:11 +0300

    Add update checker

--- templates/cli-tool/CompanyName.ProjectName.Cli/Program.cs
+++ templates/cli-tool/CompanyName.ProjectName.Cli/Program.cs
@@ -1,17 +1,20 @@
 ﻿using CliFx;
 using CompanyName.ProjectName.Cli;
 using Microsoft.Extensions.DependencyInjection;
+#if NugetUpdateCheck
+using CompanyName.ProjectName.Cli.Services;
+#endif
 
 internal class Program
 {
     private static async Task<int>  Main(string[] args) =>
-     await new CliApplicationBuilder()
-            .SetExecutableName("ProjectName")
-            .SetDescription("CLI tool for ProjectName")
-            .SetTitle("ProjectName CLI")
-            .BuildServices()
-            .Build()
-            .RunAsync();
+        await new CliApplicationBuilder()
+                .SetExecutableName("ProjectName")
+                .SetDescription("CLI tool for ProjectName")
+                .SetTitle("ProjectName CLI")
+                .BuildServices()
+                .Build()
+                .RunAsync();
 }
 
 public static class Startup
@@ -28,6 +31,12 @@ public static class Startup
 
         services.AutoRegister();
 
-        return builder.UseTypeActivator(services.BuildServiceProvider());
+        var serviceProvider = services.BuildServiceProvider();
+
+#if NugetUpdateCheck
+        serviceProvider.GetRequiredService<UpdateChecker>().CheckAsync().Wait();
+#endif
+
+        return builder.UseTypeActivator(serviceProvider);
     }
 }
